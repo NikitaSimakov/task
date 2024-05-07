004fb6098d17dcc140987bcd2d37105b7b510bdc
NikitaSimakov <mishaaniskin@gmail.com>
2024-05-07 09:17:33 +0300

    updated task_1

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,33 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+// console.log("Let's rock")
+// console.log(encoded, translations)
 
 
 
-// console.log(decoded)
+const decodeFields = (encoded, translations) => {
+  const uniqueIds = [];
+
+
+  const decodedItems = encoded.map(item => {
+    const decodedItem = {};
+    for (const key in item) {
+      if (key.endsWith('Id') && !['groupId', 'service', 'formatSize', 'ca'].includes(key)) {
+        const id = item[key];
+        decodedItem[key.replace('Id', '')] = id !== "0" ? (translations[id] || id) : "";
+        if (!uniqueIds.includes(key)) {
+          uniqueIds.push(key);
+        }
+      } else {
+        decodedItem[key] = item[key];
+      }
+
+    }
+    return decodedItem;
+  });
+
+  return [decodedItems, uniqueIds];
+};
+
+const decoded = decodeFields(encoded, translations)
+console.log(decoded)
